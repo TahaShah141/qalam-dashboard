498a31862d952ecc4b586e113ad1afa4b84942e6
TahaShah141 <66671198+TahaShah141@users.noreply.github.com>
2025-05-19 03:34:46 +0500

    dim absent more

--- components/custom/AttendanceMap.tsx
+++ components/custom/AttendanceMap.tsx
@@ -18,7 +18,7 @@ export const AttendanceMap = ({attendances, horizontal, rows}: AttendanceMapProp
     // if (i >= attendances.length) return "from-neutral-700 to-black border-neutral-800"
     // return attendances[i].isPresent ? "from-green-700 to-black border-green-500" : "from-red-700 to-black border-red-500"
     if (i >= attendances.length) return "bg-secondary/50 border-black"
-    return attendances[i].isPresent ? "bg-primary border-black" : "bg-primary/50 border-black"
+    return attendances[i].isPresent ? "bg-primary border-black" : "bg-primary/30 border-black"
   } 
 
   const [dateOpen, setDateOpen] = useState(-1)
