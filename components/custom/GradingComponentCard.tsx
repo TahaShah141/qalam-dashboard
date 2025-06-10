9b4ebe0500bb1a861dc9e848b158308811e1c301
TahaShah141 <tahashah411@gmail.com>
2025-06-11 02:05:28 +0500

    add weights to components on tooltips

--- components/custom/GradingComponentCard.tsx
+++ components/custom/GradingComponentCard.tsx
@@ -16,17 +16,21 @@ export const GradingComponentCard = ({name, weight, components}: GradingComponen
       label: "Average",
       color: "#525252",
     },
+    weight: {
+      label: "Weight",
+      color: "#525252",
+    },
   } satisfies ChartConfig
-
-  const chartData = components.map(({name, obtainedMarks, averageMarks, maxMarks}) => ({
-    name,
-    obtained: Math.round((obtainedMarks/maxMarks)*1000) / 10,
-    average: Math.round((averageMarks/maxMarks)*1000) / 10
-  })).filter(d => d.average !== 0)
 
   const totalMaxMarks = components.reduce((sum, { maxMarks }) => sum + maxMarks, 0);
   const totalObtained = components.reduce((sum, { obtainedMarks }) => sum + obtainedMarks, 0) / totalMaxMarks * 100;
   const totalAverage = components.reduce((sum, { averageMarks }) => sum + averageMarks, 0) / totalMaxMarks * 100;
+
+  const chartData = components.map(({name, obtainedMarks, averageMarks, maxMarks}) => ({
+    name: JSON.stringify({name, weight: Math.round((maxMarks/totalMaxMarks)*1000) / 10}),
+    obtained: Math.round((obtainedMarks/maxMarks)*1000) / 10,
+    average: Math.round((averageMarks/maxMarks)*1000) / 10,
+  })).filter(d => d.average !== 0)
 
   if (chartData.length < 1) return null
 
@@ -55,13 +59,33 @@ export const GradingComponentCard = ({name, weight, components}: GradingComponen
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value, i) => chartData.length <= 6 ? value : `${i+1}`}
+              tickFormatter={(value, i) => chartData.length <= 6 ? JSON.parse(value).name : `${i+1}`}
             />
             <ChartTooltip 
               cursor={false}
-              content={<ChartTooltipContent indicator="line" />}
+              content={<ChartTooltipContent indicator="line" 
+              formatter={(value, name) => (
+                <div className="text-muted-foreground flex min-w-[130px] items-center text-xs">
+                  {chartConfig[name as keyof typeof chartConfig]?.label || name}
+                  <div className="text-foreground ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums">
+                    {value}
+                    <span className="text-muted-foreground font-normal">
+                      %
+                    </span>
+                  </div>
+                </div>)} 
+              />}
+              labelFormatter={(value) => {
+                const { name, weight } = JSON.parse(value)
+                return (
+                  <div className="flex justify-between">
+                    <p>{name}</p>
+                    <p className="text-muted-foreground font-normal">{weight}%</p>
+                  </div>
+                )
+              }}
             />
-            <Bar className="w-[40px]" dataKey="obtained" fill="var(--color-obtained)" radius={chartData.length <= 6 ? 2 : 0} />
+            <Bar dataKey="obtained" fill="var(--color-obtained)" radius={chartData.length <= 6 ? 2 : 0} />
             <Bar dataKey="average" fill="var(--color-average)" radius={chartData.length <= 6 ? 2 : 0} />
           </BarChart>
         </ChartContainer>
